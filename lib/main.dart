7d0f356cf633944fd5f9d105861493d456aaa10f
nayzawaungnza <nayzawaung750@gmail.com>
2024-11-07 13:55:14 +0630

    list tile

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // const MyApp({super.key});
+  List<Contact> _list = List.generate(100, (index) {
+    return Contact(
+        name: 'Name $index', email: 'Email $index', phone: 'Phone $index');
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -51,20 +55,18 @@ class MyApp extends StatelessWidget {
               borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(30),
                   bottomRight: Radius.circular(30)))),
-      body: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          Text('Hello World'),
-          Text('Hello World'),
-          Text('Hello World'),
-          Text('Hello World'),
-          Text('Hello World'),
-          Text('Hello World'),
-          Text('Hello World'),
-          Text('Hello World'),
-          Text('Hello World'),
-        ],
-      ),
+      body: ListView.builder(
+          itemCount: _list.length,
+          itemBuilder: (BuildContext context, index) {
+            return Card(
+              child: ListTile(
+                title: Text(_list[index].name!),
+                subtitle: Text(_list[index].email!),
+                trailing: Text(_list[index].phone!),
+                leading: Icon(Icons.person),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.home),
           onPressed: () {
@@ -72,4 +74,11 @@ class MyApp extends StatelessWidget {
           }),
     );
   }
+}
+
+class Contact {
+  final String? name;
+  final String? phone;
+  final String? email;
+  Contact({this.name, this.phone, this.email});
 }
